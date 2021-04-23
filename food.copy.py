9b26ad6ab07c41cf3c109ccdc1cca69724c0bc2c
pranitha <pranitha.gunda@gmail.com>
2021-04-23 15:09:35 +0530

    food

--- food.copy.py
+++ food.copy.py
@@ -2,5 +2,7 @@ food=input("are you hungry?")
 if food=="yes":
     print("hungry")
 else:
+    hungry=input("food required")
+    if hungry=="yes":
+        print("food required")
     print("not hungry")
-print("nothing")
